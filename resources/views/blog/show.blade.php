4c29a590eba8f524b587082bf5937030523599a3
ElColillas69 <mugiwarasnpai77@gmail.com>
2024-03-10 06:30:18 +0000

    Made a go back button inside the blog posts page and also tried to implement the image inside the blog but not working fully currently

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -1,22 +1,37 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="w-4/5 m-auto text-left">
-    <div class="py-15">
-        <h1 class="text-6xl">
-            {{ $post->title }}
-        </h1>
+<main class="sm:container sm:mx-auto sm:max-w-lg sm:mt-10">
+    <div class="flex">
+        <div class="w-full">
+            <section class="flex flex-col break-words bg-white sm:border-1 sm:rounded-md sm:shadow-sm sm:shadow-lg">
+
+                <div class="blog-image">
+                    <img src="{{ asset('..../public/images/' . $post->image_path) }}" alt="Blog Image" class="w-full">
+                </div>
+
+                <div class="w-4/5 m-auto text-left py-15">
+                    <h1 class="text-6xl">
+                        {{ $post->title }}
+                    </h1>
+                </div>
+
+                <div class="w-4/5 m-auto pt-20">
+                    <span class="text-gray-500">
+                        By <span class="font-bold italic text-gray-800">{{ $post->user->name }}</span>, Created on {{ date('jS M Y', strtotime($post->updated_at)) }}
+                    </span>
+
+                    <p class="text-xl text-gray-700 pt-8 pb-10 leading-8 font-light">
+                        {{ $post->description }}
+                    </p>
+                </div>
+
+                <div class="flex justify-center mt-8">
+                    <a href="/blog" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">Go Back</a>
+                </div>
+
+            </section>
+        </div>
     </div>
-</div>
-
-<div class="w-4/5 m-auto pt-20">
-    <span class="text-gray-500">
-        By <span class="font-bold italic text-gray-800">{{ $post->user->name }}</span>, Created on {{ date('jS M Y', strtotime($post->updated_at)) }}
-    </span>
-
-    <p class="text-xl text-gray-700 pt-8 pb-10 leading-8 font-light">
-        {{ $post->description }}
-    </p>
-</div>
-
-@endsection 
+</main>
+@endsection
